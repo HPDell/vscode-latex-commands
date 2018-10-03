eb68f2f61efde04f126cdc5b7ae2d83bc3f9c948
Yigong Hu <huyg0180110559@outlook.com>
2018-10-03 10:06:32 +0800

    Add: basic inline math and display math commands.

---
+++ src/commands.ts
@@ -0,0 +1,18 @@
+interface LaTeXCommands {
+    commands: string;
+    snippts: string;
+}
+
+let commands: LaTeXCommands[] = new Array<LaTeXCommands>();
+
+commands.push({
+    commands: "latex-commands.inlinemath",
+    snippts: " $ $1 $ "
+});
+
+commands.push({
+    commands: "latex-commands.displaymath",
+    snippts: " $$ $1 $$ "
+});
+
+export default commands;
