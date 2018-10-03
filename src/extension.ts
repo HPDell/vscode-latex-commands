28035f8e3690a63dc400e9c2594c0dcc8ec141ee
Yigong Hu <huyg0180110559@outlook.com>
2018-10-03 10:09:37 +0800

    modify: some names.

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import commands from './commands';
+import commands from './snippetCommands';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,14 +16,14 @@ export function activate(context: vscode.ExtensionContext) {
     // The commandId parameter must match the command field in package.json
     for (const command in commands) {
         if (commands.hasOwnProperty(command)) {
-            const snippt = commands[command];
+            const snippet = commands[command];
             
-            let disposable = vscode.commands.registerTextEditorCommand(snippt.commands, () => {
+            let disposable = vscode.commands.registerTextEditorCommand(snippet.commands, () => {
                 // The code you place here will be executed every time your command is executed
                 let editor = vscode.window.activeTextEditor;
                 let selection : vscode.Selection = editor.selection;
                 let insertPosition = new vscode.Position(selection.active.line, 0);
-                editor.insertSnippet(new vscode.SnippetString(snippt.snippts), insertPosition);
+                editor.insertSnippet(new vscode.SnippetString(snippet.snippts), insertPosition);
             });
 
             context.subscriptions.push(disposable);
